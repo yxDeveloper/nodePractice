165a853b0a504c3624a43c0dca286883c65a6759
yxDeveloper <dilutian0415@gmail.com>
2020-08-19 18:42:20 +0800

    debug

---
+++ 02/code/02-像Apache一样.js
@@ -0,0 +1,32 @@
+var http = require('http')
+var fs = require('fs')
+
+var server = http.createServer()
+
+var wwwDir = 'D:/Movie/www'
+
+server.on('request', function (req, res) {
+  var url = req.url
+  // / index.html
+  // /a.txt wwwDir + /a.txt
+  // /apple/login.html wwwDir + /apple/login.html
+  // /img/ab1.jpg wwwDir + /img/ab1.jpg
+  
+
+  var filePath = '/index.html'
+  if (url !== '/') {
+    filePath = url
+  }
+
+  fs.readFile(wwwDir + filePath, function (err, data) {
+    if (err) {
+      return res.end('404 Not Found.')
+    }
+    res.end(data)
+  })
+})
+
+// 3. 绑定端口号，启动服务
+server.listen(3000, function () {
+  console.log('running...')
+})
