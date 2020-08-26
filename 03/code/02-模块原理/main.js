49967aabdf4db1ce0232c6f4fc2db9e8a3f070d4
yxDeveloper <dilutian0415@gmail.com>
2020-08-26 23:37:37 +0800

    main

---
+++ 03/code/02-模块原理/main.js
@@ -0,0 +1,11 @@
+var fooExports = require('./foo')
+
+console.log(fooExports)
+
+
+// 如果你实在分不清楚 exports 和 module.exports
+// 你可以选择忘记 exports
+// 而只使用 module.exports 也没问题
+// 
+// module.exports.xxx = xxx
+// moudle.exports = {}
