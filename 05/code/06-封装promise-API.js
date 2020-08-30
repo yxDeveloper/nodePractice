22e38b4d6aed1a6fef7b13ee3f3698f58703bc41
yxDeveloper <dilutian0415@gmail.com>
2020-08-30 23:48:53 +0800

    5

---
+++ 05/code/06-封装promise-API.js
@@ -0,0 +1,26 @@
+var fs = require('fs')
+
+function pReadFile(filePath) {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, 'utf8', function (err, data) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
+
+pReadFile('./data/a.txt')
+  .then(function (data) {
+    console.log(data)
+    return pReadFile('./data/b.txt')
+  })
+  .then(function (data) {
+    console.log(data)
+    return pReadFile('./data/c.txt')
+  })
+  .then(function (data) {
+    console.log(data)
+  })
